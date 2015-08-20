36758f18856a02c3c0a32458b965cc3260859552
Claus Lensbøl <cmol@cmol.dk>
2015-08-20 19:59:05 +0200

    Add script and readme

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # control-pi
 Small RaspberryPi2 control scrips
+
+
+## Installation
+Place script in `/root/control.sh` and run the command:
+
+    echo "@reboot        root    /home/control.sh" >> /etc/crontab
+
+You might need to prepend `sudo` to this command if you are not root.
+
+## Wireing
+For the (soft) reboot, wire the button to the GPIO3 pin, and to the GND next to it.
+
+For the booted LED, figure out what resistance you need for the LED (or you might burn of the board),
+and place the LED in serial with the resistor into GPIO4, and the GND "below" it.
+
+There is a GPIO overview here: https://www.raspberrypi.org/learning/quick-reaction-game/worksheet/
---
+++ control.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+if [[ $EUID -ne 0 ]]; then
+   echo "Error: This script must be run as root" >&2
+   exit 1
+fi
+
+ALIVE_PIN="2"
+SHUTDOWN_PIN="3"
+
+# Register PINS needed for reboot and alive LED
+echo "$SHUTDOWN_PIN" > /sys/class/gpio/export
+echo "in" > /sys/class/gpio/gpio"$SHUTDOWN_PIN"/direction
+echo "$ALIVE_PIN" > /sys/class/gpio/export
+echo "out" > /sys/class/gpio/gpio"$ALIVE_PIN"/direction
+
+# Run on alive LED
+echo "1" > /sys/class/gpio/gpio"$ALIVE_PIN"/value
+
+while ( true )
+do
+    # check if the pin is connected to GND and, if so, halt the system
+    if [ $(</sys/class/gpio/gpio"$SHUTDOWN_PIN"/value) == 0 ]
+    then
+        echo "0" > /sys/class/gpio/gpio"$ALIVE_PIN"/value
+        echo "$SHUTDOWN_PIN" > /sys/class/gpio/unexport
+        echo "$ALIVE_PIN" > /sys/class/gpio/unexport
+        shutdown -h now "System halted by a GPIO action"
+    fi
+    sleep 1
+done
