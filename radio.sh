42e135f7bd498fba5ff158f98d6aa8f092517595
Claus Lensbøl <cmol@cmol.dk>
2016-01-24 12:59:35 +0100

    Add test for a broken stream via dbus

--- radio.sh
+++ radio.sh
@@ -2,9 +2,18 @@
 
 RADIO="http://playerservices.streamtheworld.com/pls/CBC_R1_EDM_H.pls"
 
-if [[ -z "$(pgrep vlc)" ]] ;
+if [[ -z "$(pgrep vlc)" ]]
 then
-  nohup cvlc $RADIO &>/dev/null &
-  #cvlc $RADIO
+  nohup cvlc $RADIO --control dbus &>/dev/null &
+else
+  p=$(pgrep vlc)
+  DBUS_SESSION_BUS_ADDRESS=$(xargs -n 1 -0 < /proc/$p/environ | sed -n 's/^DBUS_SESSION_BUS_ADDRESS=\(.*\)/\1/p')
+  p1=$(qdbus org.mpris.MediaPlayer2.vlc /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Position)
+  sleep 30
+  p2=$(qdbus org.mpris.MediaPlayer2.vlc /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Position)
+  if [[ $p1 -eq $p2 ]]
+  then
+    pkill vlc
+    nohup cvlc $RADIO --control dbus &>/dev/null &
+  fi
 fi
-
